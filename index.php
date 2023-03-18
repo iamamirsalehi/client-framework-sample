0c11c8f7221d5ed6b351231fd499aeab3955d900
Amir Salehi <iamamirsalehi@gmail.com>
2023-03-18 18:15:59 +0330

    index.php updated

--- index.php
+++ index.php
@@ -4,7 +4,13 @@ include_once __DIR__ . "/vendor/autoload.php";
 $router = new \Ls\ClientAssistant\Core\Router();
 $router->setViewsDirectoryAddress((__DIR__ . DIRECTORY_SEPARATOR . 'views'));
 
+include_once __DIR__ . '/routes/auth.php';
+include_once __DIR__ . '/routes/blog.php';
+include_once __DIR__ . '/routes/cart.php';
 include_once __DIR__ . '/routes/lms.php';
+include_once __DIR__ . '/routes/pages.php';
+include_once __DIR__ . '/routes/panel.php';
+include_once __DIR__ . '/routes/shop.php';
 
 $match = $router->match();
 
