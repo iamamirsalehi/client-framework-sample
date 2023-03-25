bfa7524e5d1357d43cdc084112715ae033edfc93
Amir Salehi <iamamirsalehi@gmail.com>
2023-03-25 15:55:58 +0330

    routes files updated

--- app/helpers.php
+++ app/helpers.php
@@ -4,21 +4,21 @@
 if (!function_exists('asset_url')) {
     function asset_url(string $fileAddress)
     {
-        $assetsPath = __DIR__ . DIRECTORY_SEPARATOR . '../assets' . DIRECTORY_SEPARATOR;
+        $assetsPath = realpath(__DIR__ . DIRECTORY_SEPARATOR . '../assets' . DIRECTORY_SEPARATOR);
 
-        return ($_ENV['APP_URL'] ?? '') . $assetsPath . $fileAddress;
+        return ($_ENV['APP_URL'] ?? '') . $assetsPath . DIRECTORY_SEPARATOR . $fileAddress;
     }
 }
 
 if (!function_exists('view')) {
     function view(string $view, array $vars = [])
     {
-        $viewsPath = __DIR__ . DIRECTORY_SEPARATOR . '../views' . DIRECTORY_SEPARATOR;
+        $viewsPath = realpath(__DIR__ . DIRECTORY_SEPARATOR . '../views' . DIRECTORY_SEPARATOR);
 
         if (!empty($vars)) {
             extract($vars);
         }
 
-        return include $viewsPath . str_replace('.', DIRECTORY_SEPARATOR, $view) . '.php';
+        return include $viewsPath . DIRECTORY_SEPARATOR . str_replace('.', DIRECTORY_SEPARATOR, $view) . '.php';
     }
 }
