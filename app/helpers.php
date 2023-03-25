ccca6c9ccce2125d650a0fa148ae951d90ba9d9c
Amir Salehi <iamamirsalehi@gmail.com>
2023-03-25 19:26:06 +0330

    asset_url helper updated

--- app/helpers.php
+++ app/helpers.php
@@ -4,9 +4,7 @@
 if (!function_exists('asset_url')) {
     function asset_url(string $fileAddress)
     {
-        $assetsPath = realpath(__DIR__ . DIRECTORY_SEPARATOR . '../assets' . DIRECTORY_SEPARATOR);
-
-        return ($_ENV['APP_URL'] ?? '') . $assetsPath . DIRECTORY_SEPARATOR . $fileAddress;
+        return ($_ENV['APP_URL'] ?? '') . '/assets/' . $fileAddress;
     }
 }
 
